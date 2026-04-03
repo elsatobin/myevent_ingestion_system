4d503150c9c4fea55d87edac093e843fef5edd77
elsatobin <elsa0010021@outlook.com>
2026-04-03 13:53:08 +0300

    delete unneccessary file

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,6 @@
-import asyncio
 from app.db import get_engine
 from app.models import Base
 
 async def init_db():
     async with get_engine().begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
-
-if __name__ == "__main__":
-    asyncio.run(init_db())
